225a588416e5441d291757304219d919d86ad242
Gulei Hu <gulei.hu@gmail.com>
2018-10-28 03:16:41 +0800

    draft DomTooManyStrongRule

--- Rules/BaseRule.js
+++ Rules/BaseRule.js
@@ -1,4 +1,12 @@
 class BaseRule {
+  constructor(params = {}) {
+    this._params = params;
+  }
+
+  get params() {
+    return this._params;
+  }
+
   validate(context) {
     throw 'Method not implemented: validate';
   }
