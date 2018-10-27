48fae62c8c823acdee0d500c688a896b4b57df24
Gulei Hu <gulei.hu@gmail.com>
2018-10-28 02:27:10 +0800

    draft DomAMissingRelRule

---
+++ Rules/DomAMissingRelRule.js
@@ -0,0 +1,27 @@
+const BaseRule = require('./BaseRule');
+
+class DomAMissingRelRule extends BaseRule {
+  validate({evaluators}) {
+    const $ = evaluators.cheerio;
+
+    let count = 0;
+
+    $('a').each((i, a) => {
+      const rel = $(a).attr('rel');
+
+      if (rel.trim().length < 1) {
+        count++;
+      }
+    });
+
+    if (count < 1) {
+      return [];
+    }
+
+    return [
+      `Count of <a/> missing rel: ${count}`,
+    ];
+  }
+}
+
+module.exports = DomAMissingRelRule;
