c498f59719e3673a712d436d32118f0df8b17bc0
Gulei Hu <gulei.hu@gmail.com>
2018-10-28 12:40:35 +0800

    draft DomHeadCheckRule

---
+++ Rules/DomHeadCheckRule.test.js
@@ -0,0 +1,89 @@
+const DomHeadCheckRule = require('./DomHeadCheckRule');
+const helper = require('./helpers');
+
+test('validate:valid', () => {
+  const htmlValid = `
+<html>
+<head>
+  <title>TITLE</title>
+  <meta name="description" content="META_DESC">
+  <meta name="keywords" content="META_KEYWORDS">
+</head>
+<body></body>
+</html>
+`;
+
+  const defects = helper.validate(htmlValid, new DomHeadCheckRule());
+
+  expect(defects).toEqual([]);
+});
+
+test('validate:invalid:missing-title', () => {
+  const htmlInvalid = `
+<html>
+<head>
+  <meta name="description" content="META_DESC">
+  <meta name="keywords" content="META_KEYWORDS">
+</head>
+<body></body>
+</html>  
+`;
+
+  const defects = helper.validate(htmlInvalid, new DomHeadCheckRule());
+
+  expect(defects.length).toBe(1);
+  expect(defects[0] = 'Missing <title/> in <head/>');
+});
+
+test('validate:invalid:missing-meta-description', () => {
+  const htmlInvalid = `
+<html>
+<head>
+  <title>TITLE</title>
+  <meta name="keywords" content="META_KEYWORDS">
+</head>
+<body></body>
+</html>  
+`;
+
+  const defects = helper.validate(htmlInvalid, new DomHeadCheckRule());
+
+  expect(defects.length).toBe(1);
+  expect(defects[0] = 'Missing <meta name="description"/> in <head/>');
+});
+
+test('validate:invalid:missing-meta-keywords', () => {
+  const htmlInvalid = `
+<html>
+<head>
+  <title>TITLE</title>
+  <meta name="description" content="META_DESC">
+</head>
+<body></body>
+</html>  
+`;
+
+  const defects = helper.validate(htmlInvalid, new DomHeadCheckRule());
+
+  expect(defects.length).toBe(1);
+  expect(defects[0] = 'Missing <meta name="keywords"/> in <head/>');
+});
+
+test('validate:invalid:missing-all', () => {
+  const htmlInvalid = `
+<html>
+<head></head>
+<body></body>
+</html>  
+`;
+
+  const defects = helper.validate(htmlInvalid, new DomHeadCheckRule());
+  const expected = [
+    'Missing <title/> in <head/>',
+    'Missing <meta name="description"/> in <head/>',
+    'Missing <meta name="keywords"/> in <head/>',
+  ];
+
+  expect(defects.length).toBe(3);
+  expect(defects).toEqual(expect.arrayContaining(expected))
+});
