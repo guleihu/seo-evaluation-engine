a6f9aba4c46f60b8d19c331c7555d54c205ed5d5
Gulei Hu <gulei.hu@gmail.com>
2018-10-29 01:22:50 +0800

    changed to use https for pushing to github

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy docs'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:guleihu/seo-evaluation-engine.git master:gh-pages
+git push -f https://github.com/guleihu/seo-evaluation-engine.git master:gh-pages
 
 cd -
